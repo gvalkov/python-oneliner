f23f7fcdd6cef598517ddbe45c5e9fca6e8b802f
Georgi Valkov <georgi.t.valkov@gmail.com>
2014-09-24 01:01:42 +0200

    convert to py.test

--- tests.py
+++ tests.py
@@ -1,142 +1,88 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; -*-
 
-from unittest import TestCase, main as unitmain
+from pytest import mark
 from oneliner import *
 
+
+def test_module_finder():
+    mic = modules_in_code
+    assert mic('write(sys.version)') == ['sys']
+    assert mic('abc.write sys.version.none') == ['abc', 'sys']
+    assert mic(';sys.stdout.write();') == ['sys']
+
+def test_module_split():
+    pms = parse_modules_split
+    assert pms('os,sys,re')    == ['os', 'sys', 're']
+    assert pms('os, sys, re')  == ['os', 'sys', 're']
+    assert pms('os,sys, re,')  == ['os', 'sys', 're']
+    assert pms('os.path,sys,') == ['os.path', 'sys']
+
+    assert pms('os.path.[exists]')      == ['os.path.[exists]']
+    assert pms('os.path.[exists,join]') == ['os.path.[exists,join]']
+    assert pms('sys,os.path.[exists]')  == ['sys', 'os.path.[exists]']
+    assert pms('sys,os.path.[*]')       == ['sys', 'os.path.[*]']
+    assert pms('sys,os.path.[exists,join],re') == ['sys', 'os.path.[exists,join]', 're']
+
+def test_parse_modules():
+    pm = parse_modules
+    assert pm('subprocess')     == [( ('subprocess', ''), [] )]
+    assert pm('subprocess=sub') == [( ('subprocess', 'sub'), [] )]
+    assert pm('sys=s,os=o')     == [( ('sys', 's'), [] ), ( ('os', 'o'), [] )]
+
+    assert pm('os.path.[exists]')          == [( ('os.path', ""), [('exists', '')])]
+    assert pm('os.path.[exists,join]')     == [( ('os.path', ""), [('exists', ''), ('join', '')] )]
+    assert pm('os.path.[exists=e,join=j]') == [( ('os.path', ""), [('exists', 'e'), ('join', 'j')] )]
+    assert pm('sys,os.path.[*]')           == [( ('sys', ''), [] ), ( ('os.path', ''), [('*', '')] )]
+
+def test_import_modules():
+    im = lambda x: sorted(import_modules(parse_modules(x)))
+    assert im('sys,os,re') == ['os', 're', 'sys']
+    assert im('subprocess=sub') == ['sub']
+    assert im('subprocess=sub,sys') == ['sub', 'sys']
+    assert im('sys=a,os=b,re=c') == ['a', 'b', 'c']
+
+    assert im('os.path.[exists]') == ['exists']
+    assert im('sys=s,os.path.[exists]') == ['exists', 's']
+    assert im('os.path.[exists,join]') == ['exists', 'join']
+    assert im('os.path.[exists=e,join=j]') == ['e', 'j']
+
+def test_import_modules_wildcard():
+    res = import_modules(parse_modules('os.path.[*]'))
+    assert all([i in res for i in ['join', 'isdir', 'abspath']])
+
+def test_default_list():
+    l = defaultlist([1,2,3])
+    assert l[0] == 1
+    assert l[10] == None
+
+    l = defaultlist([1,2,3], default=True)
+    assert l[10] == True
+
+def test_functional():
+    data, args = 'example', ['-ne', 'line.upper()']
+    assert run(args, data) == 'EXAMPLE'
+
+    data, args = 'example', ['-ns', 'print(line.upper())']
+    assert run(args, data) == 'EXAMPLE\n'
+
+    data = '1355256353'
+    args = ['-j', ' => ', '-line' '_, datetime.datetime.fromtimestamp(int(_))']
+    assert run(args, data) == '1355256353 => 2012-12-11 21:05:53\n'
+
+#-----------------------------------------------------------------------------
+# utility functions
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
 
-
 def stdinstr(s):
-    fh = StringIO(s); fh.isatty = lambda: False ; return fh
+    fh = StringIO(s)
+    fh.isatty = lambda: False
+    return fh
 
-
-class TestModuleFinder(TestCase):
-    tests = {
-        'write(sys.version)':         ['sys'],
-        'abc.write sys.version.none': ['abc', 'sys'],
-        ';sys.stdout.write();':       ['sys'],
-        }
-
-    def test(self):
-        for code, res in self.tests.items():
-            mods = modules_in_code(code)
-            self.assertEqual(mods, res)
-
-
-class TestModuleParser(TestCase):
-    def dotests(self, tests, cb):
-        for line, res in tests.items():
-            mods = cb(line)
-            self.assertEqual(mods, res)
-
-    def test_simple_split(self):
-        tests = {
-            'os,sys,re':    ['os', 'sys', 're'],
-            'os, sys, re':  ['os', 'sys', 're'],
-            'os,sys, re,':  ['os', 'sys', 're'],
-            'os.path,sys,': ['os.path', 'sys'],
-            }
-
-        self.dotests(tests, parse_modules_split)
-
-    def test_brackets_split(self):
-        tests = {
-            'os.path.[exists]':      ['os.path.[exists]'],
-            'os.path.[exists,join]': ['os.path.[exists,join]'],
-            'sys,os.path.[exists]':  ['sys', 'os.path.[exists]'],
-            'sys,os.path.[*]':       ['sys', 'os.path.[*]'],
-            'sys,os.path.[exists,join],re' : ['sys', 'os.path.[exists,join]', 're'],
-            }
-
-        self.dotests(tests, parse_modules_split)
-
-    def test_simple_locals(self):
-        tests = {
-            'subprocess':     [( ('subprocess', ''), [] )],
-            'subprocess=sub': [( ('subprocess', 'sub'), [] )],
-            'sys=s,os=o':     [( ('sys', 's'), [] ), ( ('os', 'o'), [] )],
-            }
-
-        self.dotests(tests, parse_modules)
-
-    def test_brackets(self):
-        tests = {
-            'os.path.[exists]':
-                [( ('os.path', ""), [('exists', '')])],
-            'os.path.[exists,join]':
-                [( ('os.path', ""), [('exists', ''), ('join', '')])],
-            'os.path.[exists=e,join=j]':
-                [( ('os.path', ""), [('exists', 'e'), ('join', 'j')])],
-            'sys,os.path.[*]':
-                [( ('sys', ''), [] ), ( ('os.path', ''), [('*', '')] )]
-            }
-
-        self.dotests(tests, parse_modules)
-
-
-class TestModuleImport(TestCase):
-    def dotests(self, tests, cb):
-        for line, expect in tests.items():
-            res = cb(parse_modules(line))
-            self.assertEqual(sorted(res.keys()), sorted(expect))
-
-    def test_simple(self):
-        tests = {
-            'sys,os,re': ['os', 're', 'sys'],
-            'subprocess=sub': ['sub'],
-            'subprocess=sub,sys': ['sub', 'sys'],
-            'sys=a,os=b,re=c': ['a', 'b', 'c'],
-            }
-
-        self.dotests(tests, import_modules)
-
-    def test_brackets(self):
-        tests = {
-            'os.path.[exists]': ['exists'],
-            'sys=s,os.path.[exists]': ['exists', 's'],
-            'os.path.[exists,join]': ['exists', 'join'],
-            'os.path.[exists=e,join=j]': ['e', 'j'],
-            }
-
-        self.dotests(tests, import_modules)
-
-    def test_wildcard(self):
-        res = import_modules(parse_modules('os.path.[*]'))
-        for i in 'join', 'isdir', 'abspath':
-            self.assertTrue(i in res)
-
-
-class TestDefaultList(TestCase):
-    def test_access(self):
-        l = defaultlist([1,2,3])
-        self.assertEqual(l[0], 1)
-        self.assertEqual(l[10], None)
-
-        l = defaultlist([1,2,3], default=True)
-        self.assertEqual(l[10], True)
-
-
-class TestFunctional(TestCase):
-    tests_single = {
-        'EXAMPLE':   ( ['-ne', 'line.upper()'], 'example' ),
-        'EXAMPLE\n': ( ['-ns', 'print(line.upper())'], 'example' ),
-        # will fail in different time zones
-        '1355256353 => 2012-12-11 21:05:53\n': (
-            ['-j', ' => ', '-line' '_, datetime.datetime.fromtimestamp(int(_))'], '1355256353'),
-    }
-
-    def test(self):
-        '''Test if the examples from the documentation work.'''
-
-        for expect, args in self.tests_single.items():
-            res = StringIO()
-            main(args[0], stdinstr(args[1]), fh_out=res)
-            self.assertEqual(res.getvalue(), expect)
-
-
-if __name__ == '__main__':
-    unitmain()
+def run(args, data):
+    res = StringIO()
+    main(args, stdinstr(data), fh_out=res)
+    return res.getvalue()

c43a182e2576d202cceb4c8b93689b3cc1e11ec7
Georgi Valkov <georgi.t.valkov@gmail.com>
2024-02-18 18:25:07 +0100

    ruff format

--- oneliner.py
+++ oneliner.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8; -*-
-
-from __future__ import print_function
 
 import os
 import re
@@ -10,21 +7,21 @@ import argparse
 
 from sys import stderr, stdout, stdin
 
-__version__ = '0.2.1'
+__version__ = "0.2.1"
 
 
-usage_short = 'Usage: {} [-hvnpdijl] [-m mod] [-e expr] [-s stmt] <path> [<path> ...]'
-usage_short = usage_short.format('pyl' if 'pyl' in sys.argv[0] else 'python -m oneliner')
+usage_short = "Usage: {} [-hvnpdijl] [-m mod] [-e expr] [-s stmt] <path> [<path> ...]"
+usage_short = usage_short.format("pyl" if "pyl" in sys.argv[0] else "python3 -m oneliner")
 
-usage = '''\
-%s
+usage = f"""
+{usage_short}
 
 Options:
   -h       show this help message and exit
   -v       show version and exit
   -n       run statement or expression for each line of input
   -p       like -n, but print value of 'line' every input cycle
-  -d       field delimiter regex (default: '\s+')
+  -d       field delimiter regex (default: '\\s+')
   -i       attempt to automatically import module names
   -j       join tuples or lists before printing
   -l       chomp newlines from input
@@ -80,15 +77,16 @@ Importing Modules:
   'time' module will be imported automatically:
 
     yes | pyl -j -line '(time.time(), line)'
-''' % usage_short
+"""
 
 
 # Modules that are available to one-liners by default.
-provided_modules = ['os', 're', 'sys']
+provided_modules = ["os", "re", "sys"]
 
 
 class defaultlist(list):
-    '''A list that returns a default value on IndexErrors.'''
+    """A list that returns a default value on IndexErrors."""
+
     def __init__(self, iterable, default=None):
         list.__init__(self, iterable)
         self._default = default
@@ -101,24 +99,24 @@ class defaultlist(list):
 
 
 def parse_args(argv, fh_in):
-    '''Parse arguments and do a basic sanity check.'''
+    """Parse arguments and do a basic sanity check."""
     parser = argparse.ArgumentParser(add_help=False)
     o = parser.add_argument
-    version = '%(prog)s version {}'.format(__version__)
+    version = "%(prog)s version {}".format(__version__)
 
-    o('-h', '--help', action='store_true')
-    o('-v', '--version', action='version', version=version)
-    o('-n', action='store_true', dest='readloop')
-    o('-p', action='store_true', dest='printloop')
-    o('-l', action='store_true', dest='chomp')
-    o('-i', action='store_true', dest='autoimports')
-    o('-d', default=r'\s+', dest='fssep')
-    o('-j', nargs='?', dest='joinsep', default='', const=' ')
-    o('-m', nargs='*', dest='mods', default=[])
-    o('-e', nargs=1, dest='expr', default=[])
-    o('-s', nargs=1, dest='stmt', default=[])
-    o('--debug', action='store_true')
-    o('inputs',  nargs='*', type=argparse.FileType('r'))
+    o("-h", "--help", action="store_true")
+    o("-v", "--version", action="version", version=version)
+    o("-n", action="store_true", dest="readloop")
+    o("-p", action="store_true", dest="printloop")
+    o("-l", action="store_true", dest="chomp")
+    o("-i", action="store_true", dest="autoimports")
+    o("-d", default=r"\s+", dest="fssep")
+    o("-j", nargs="?", dest="joinsep", default="", const=" ")
+    o("-m", nargs="*", dest="mods", default=[])
+    o("-e", nargs=1, dest="expr", default=[])
+    o("-s", nargs=1, dest="stmt", default=[])
+    o("--debug", action="store_true")
+    o("inputs", nargs="*", type=argparse.FileType("r"))
 
     parser.print_usage = lambda file: print(usage, file=file)
     opts = parser.parse_args(argv)
@@ -130,23 +128,23 @@ def parse_args(argv, fh_in):
     try:
         err = Exception
         if fh_in.closed and not opts.inputs:
-            raise err('no input files or input on stdin')
+            raise err("no input files or input on stdin")
         if not fh_in.closed and not fh_in.isatty() and opts.inputs:
-            raise err('multiple input sources (stdin and command line)')
+            raise err("multiple input sources (stdin and command line)")
         if opts.expr and opts.stmt:
-            raise err('cannot use expression and statement oneliners at the same time')
+            raise err("cannot use expression and statement oneliners at the same time")
         if not opts.expr and not opts.stmt:
-            raise err('error: no expression or statement specified')
+            raise err("error: no expression or statement specified")
     except Exception as e:
         print(usage_short, file=stderr)
-        print('error: %s' % e, file=stderr)
+        print("error: %s" % e, file=stderr)
         sys.exit(1)
 
     return opts
 
 
 def parse_modules_split(line):
-    '''
+    """
     Split a comma separated list of module names, excluding commas
     between brackets.
 
@@ -155,36 +153,36 @@ def parse_modules_split(line):
 
     >>> parse_modules_split('sys,os.path.[exists,join],re')
     ['sys', 'os.path.[exists,join]', 're']
-    '''
+    """
 
-    if '[' not in line:
+    if "[" not in line:
         # os,sys,re -> ['os', 'sys', 're']
-        mods = [i for i in re.split(r'[,\s]', line) if i]
+        mods = [i for i in re.split(r"[,\s]", line) if i]
     else:
         # sys,os.path.[join,exists] -> ['sys', 'os.path.[join,exists]']
         mods = []
 
         # Positions of matching '[.*]' pairs within line
         # '0[23]5[78] -> [1,2,3,4,6,7,8,9]
-        brackets = re.finditer(r'\[[^\]]*\]', line)
+        brackets = re.finditer(r"\[[^\]]*\]", line)
         brackets = [range(*m.span()) for m in brackets]
         brackets = [j for i in brackets for j in i]
 
-        commas = list(re.finditer(r',', line))
+        commas = list(re.finditer(r",", line))
 
         line = list(line)
         for m in commas:
             pos = m.start()
             if pos not in brackets:
-                line[pos] = '$'
+                line[pos] = "$"
 
-        mods = ''.join(line).split('$')
+        mods = "".join(line).split("$")
 
     return mods
 
 
 def parse_modules(line):
-    '''Parse shorthand import statements.
+    """Parse shorthand import statements.
 
     >>> parse_modules('os.path.[exists=e,join=j]')
     [(('os.path', ''), [('exists', 'e'), ('join', 'j')])]
@@ -194,26 +192,26 @@ def parse_modules(line):
 
     >>> parse_modules('subprocess=sub', 'sys')
     [(('subprocess', 'sub'), []), (('sys', ''), [])]
-    '''
+    """
 
     mods = parse_modules_split(line)
     imports = []
 
     for mod in mods:
         # 'os.path.[exists,join]' -> ['os.path', ['exists', 'join']]
-        if '[' in mod:
-            mod, names, _ = re.split('\.?\[(.*)\]', mod)
-            names = names.split(',')
+        if "[" in mod:
+            mod, names, _ = re.split(r"\.?\[(.*)\]", mod)
+            names = names.split(",")
         else:
             mod, names = mod, []
 
         name_local = []
         for name in names:
             # 'datetime=dt' -> ['datetime', '=', 'dt']
-            name, _, local = name.partition('=')
+            name, _, local = name.partition("=")
             name_local.append((name, local))
 
-        mod, _, local = mod.partition('=')
+        mod, _, local = mod.partition("=")
 
         imports.append(((mod, local), name_local))
 
@@ -228,7 +226,7 @@ def safe_import(*args, **kw):
 
 
 def import_modules(imports):
-    '''Import the results of parse_modules().'''
+    """Import the results of parse_modules()."""
     ctx = {}
 
     for module, names in imports:
@@ -243,13 +241,13 @@ def import_modules(imports):
 
         tnames = [i[0] for i in names]
 
-        if '*' not in tnames:
+        if "*" not in tnames:
             tmp = safe_import(module, g, l, tnames, 0)
             for name, name_local in names:
                 name_local = name_local if name_local else name
                 ctx[name_local] = getattr(tmp, name)
         else:
-            tmp = safe_import(module, g, l, ['*'], 0)
+            tmp = safe_import(module, g, l, ["*"], 0)
             ctx.update(tmp.__dict__)
 
     return ctx
@@ -265,8 +263,8 @@ def parse_import_modules(mods):
 
 
 def modules_in_code(expr):
-    '''Get module names used in an expression or statement.'''
-    return re.findall(r'(?<!\.)(\b\w+)\.', expr)
+    """Get module names used in an expression or statement."""
+    return re.findall(r"(?<!\.)(\b\w+)\.", expr)
 
 
 def main(argv=sys.argv[1:], fh_in=stdin, fh_out=stdout):
@@ -274,6 +272,7 @@ def main(argv=sys.argv[1:], fh_in=stdin, fh_out=stdout):
 
     if opts.inputs:
         from fileinput import FileInput
+
         fh_in = FileInput(i.name for i in opts.inputs)
 
     stmt, expr, mods = opts.stmt, opts.expr, opts.mods
@@ -283,18 +282,19 @@ def main(argv=sys.argv[1:], fh_in=stdin, fh_out=stdout):
 def _main(expr, stmt, mods, opts, fh_in, fh_out):
     if opts.autoimports:
         from itertools import chain
+
         for i in (j for j in chain(expr, stmt) if j):
             mods.extend(modules_in_code(i))
 
     mods.extend(provided_modules)
     ctx = parse_import_modules(mods)
-    ctx['sys'].stdout = fh_out
+    ctx["sys"].stdout = fh_out
 
     try:
-        code_expr = [compile(i, '<expr>', 'eval') for i in expr if i]
-        code_stmt = [compile(i, '<stmt>', 'exec') for i in stmt if i]
+        code_expr = [compile(i, "<expr>", "eval") for i in expr if i]
+        code_stmt = [compile(i, "<stmt>", "exec") for i in stmt if i]
     except SyntaxError as e:
-        msg = 'syntax error in: %s'
+        msg = "syntax error in: %s"
         print(msg % e.text.strip(), file=stderr)
         sys.exit(1)
 
@@ -305,7 +305,7 @@ def _main(expr, stmt, mods, opts, fh_in, fh_out):
 def nloop(code_expr, code_stmt, opts, ctx, fh_in, fh_out):
     re_delim = re.compile(opts.fssep)
     joinsep = opts.joinsep
-    hasfn = hasattr(fh_in, 'filename')
+    hasfn = hasattr(fh_in, "filename")
 
     if code_expr:
         code_objects = code_expr
@@ -317,17 +317,17 @@ def nloop(code_expr, code_stmt, opts, ctx, fh_in, fh_out):
     for nr, line in enumerate(fh_in):
         line = line.strip(os.linesep) if opts.chomp else line
         words = [i.strip() for i in re_delim.split(line) if i]
-        words = defaultlist(words, default='')
+        words = defaultlist(words, default="")
 
         eval_globals = ctx.copy()
         eval_locals = {
-            'line': line,
-            'words': words,
-            'NR': nr + 1,
-            'L': line,
-            '_': line,
-            'W': words,
-            'FN': fh_in.filename() if hasfn else '<stdin>',
+            "line": line,
+            "words": words,
+            "NR": nr + 1,
+            "L": line,
+            "_": line,
+            "W": words,
+            "FN": fh_in.filename() if hasfn else "<stdin>",
         }
 
         for i in code_objects:
@@ -336,15 +336,15 @@ def nloop(code_expr, code_stmt, opts, ctx, fh_in, fh_out):
             if res and isexpr:
                 if joinsep and isinstance(res, (tuple, list)):
                     res = joinsep.join(map(str, res))
-                eval_locals['line'] = res
+                eval_locals["line"] = res
                 fh_out.write(str(res))
                 if opts.chomp:
                     fh_out.write(os.linesep)
 
         if opts.printloop:
-            res = str(eval_locals['line'])
+            res = str(eval_locals["line"])
             fh_out.write(res)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:], stdin, stdout)
--- tests/test_pyl.py
+++ tests/test_pyl.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8; -*-
 
 from pytest import mark
 from oneliner import *
@@ -7,80 +6,89 @@ from oneliner import *
 
 def test_module_finder():
     mic = modules_in_code
-    assert mic('write(sys.version)') == ['sys']
-    assert mic('abc.write sys.version.none') == ['abc', 'sys']
-    assert mic(';sys.stdout.write();') == ['sys']
+    assert mic("write(sys.version)") == ["sys"]
+    assert mic("abc.write sys.version.none") == ["abc", "sys"]
+    assert mic(";sys.stdout.write();") == ["sys"]
+
 
 def test_module_split():
     pms = parse_modules_split
-    assert pms('os,sys,re')    == ['os', 'sys', 're']
-    assert pms('os, sys, re')  == ['os', 'sys', 're']
-    assert pms('os,sys, re,')  == ['os', 'sys', 're']
-    assert pms('os.path,sys,') == ['os.path', 'sys']
+    assert pms("os,sys,re") == ["os", "sys", "re"]
+    assert pms("os, sys, re") == ["os", "sys", "re"]
+    assert pms("os,sys, re,") == ["os", "sys", "re"]
+    assert pms("os.path,sys,") == ["os.path", "sys"]
 
-    assert pms('os.path.[exists]')      == ['os.path.[exists]']
-    assert pms('os.path.[exists,join]') == ['os.path.[exists,join]']
-    assert pms('sys,os.path.[exists]')  == ['sys', 'os.path.[exists]']
-    assert pms('sys,os.path.[*]')       == ['sys', 'os.path.[*]']
-    assert pms('sys,os.path.[exists,join],re') == ['sys', 'os.path.[exists,join]', 're']
+    assert pms("os.path.[exists]") == ["os.path.[exists]"]
+    assert pms("os.path.[exists,join]") == ["os.path.[exists,join]"]
+    assert pms("sys,os.path.[exists]") == ["sys", "os.path.[exists]"]
+    assert pms("sys,os.path.[*]") == ["sys", "os.path.[*]"]
+    assert pms("sys,os.path.[exists,join],re") == ["sys", "os.path.[exists,join]", "re"]
+
 
 def test_parse_modules():
     pm = parse_modules
-    assert pm('subprocess')     == [( ('subprocess', ''), [] )]
-    assert pm('subprocess=sub') == [( ('subprocess', 'sub'), [] )]
-    assert pm('sys=s,os=o')     == [( ('sys', 's'), [] ), ( ('os', 'o'), [] )]
+    assert pm("subprocess") == [(("subprocess", ""), [])]
+    assert pm("subprocess=sub") == [(("subprocess", "sub"), [])]
+    assert pm("sys=s,os=o") == [(("sys", "s"), []), (("os", "o"), [])]
 
-    assert pm('os.path.[exists]')          == [( ('os.path', ""), [('exists', '')])]
-    assert pm('os.path.[exists,join]')     == [( ('os.path', ""), [('exists', ''), ('join', '')] )]
-    assert pm('os.path.[exists=e,join=j]') == [( ('os.path', ""), [('exists', 'e'), ('join', 'j')] )]
-    assert pm('sys,os.path.[*]')           == [( ('sys', ''), [] ), ( ('os.path', ''), [('*', '')] )]
+    assert pm("os.path.[exists]") == [(("os.path", ""), [("exists", "")])]
+    assert pm("os.path.[exists,join]") == [(("os.path", ""), [("exists", ""), ("join", "")])]
+    assert pm("os.path.[exists=e,join=j]") == [(("os.path", ""), [("exists", "e"), ("join", "j")])]
+    assert pm("sys,os.path.[*]") == [(("sys", ""), []), (("os.path", ""), [("*", "")])]
+
 
 def test_import_modules():
     im = lambda x: sorted(import_modules(parse_modules(x)))
-    assert im('sys,os,re') == ['os', 're', 'sys']
-    assert im('subprocess=sub') == ['sub']
-    assert im('subprocess=sub,sys') == ['sub', 'sys']
-    assert im('sys=a,os=b,re=c') == ['a', 'b', 'c']
+    assert im("sys,os,re") == ["os", "re", "sys"]
+    assert im("subprocess=sub") == ["sub"]
+    assert im("subprocess=sub,sys") == ["sub", "sys"]
+    assert im("sys=a,os=b,re=c") == ["a", "b", "c"]
 
-    assert im('os.path.[exists]') == ['exists']
-    assert im('sys=s,os.path.[exists]') == ['exists', 's']
-    assert im('os.path.[exists,join]') == ['exists', 'join']
-    assert im('os.path.[exists=e,join=j]') == ['e', 'j']
+    assert im("os.path.[exists]") == ["exists"]
+    assert im("sys=s,os.path.[exists]") == ["exists", "s"]
+    assert im("os.path.[exists,join]") == ["exists", "join"]
+    assert im("os.path.[exists=e,join=j]") == ["e", "j"]
+
 
 def test_import_modules_wildcard():
-    res = import_modules(parse_modules('os.path.[*]'))
-    assert all([i in res for i in ['join', 'isdir', 'abspath']])
+    res = import_modules(parse_modules("os.path.[*]"))
+    assert all([i in res for i in ["join", "isdir", "abspath"]])
+
 
 def test_default_list():
-    l = defaultlist([1,2,3])
+    l = defaultlist([1, 2, 3])
     assert l[0] == 1
     assert l[10] == None
 
-    l = defaultlist([1,2,3], default=True)
+    l = defaultlist([1, 2, 3], default=True)
     assert l[10] == True
 
+
 def test_functional():
-    data, args = 'example', ['-ne', 'line.upper()']
-    assert run(args, data) == 'EXAMPLE'
+    data, args = "example", ["-ne", "line.upper()"]
+    assert run(args, data) == "EXAMPLE"
 
-    data, args = 'example', ['-ns', 'print(line.upper())']
-    assert run(args, data) == 'EXAMPLE\n'
+    data, args = "example", ["-ns", "print(line.upper())"]
+    assert run(args, data) == "EXAMPLE\n"
 
-    data = '1355256353'
-    args = ['-j', ' => ', '-line' '_, datetime.datetime.fromtimestamp(int(_))']
-    assert run(args, data) == '1355256353 => 2012-12-11 21:05:53\n'
+    data = "1355256353"
+    args = ["-j", " => ", "-line" "_, datetime.datetime.fromtimestamp(int(_))"]
+    assert run(args, data) == "1355256353 => 2012-12-11 21:05:53\n"
 
-#-----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
 # utility functions
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
 
+
 def stdinstr(s):
     fh = StringIO(s)
     fh.isatty = lambda: False
     return fh
+
 
 def run(args, data):
     res = StringIO()

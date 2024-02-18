5359b11e3a3b0722d24b909ff33982025770f0c3
Georgi Valkov <georgi.t.valkov@gmail.com>
2024-02-18 18:35:06 +0100

    Bump version: 0.2.2 → 0.3.0

--- oneliner.py
+++ oneliner.py
@@ -7,7 +7,7 @@ import argparse
 
 from sys import stderr, stdout, stdin
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 
 
 usage_short = "Usage: {} [-hvnpdijl] [-m mod] [-e expr] [-s stmt] <path> [<path> ...]"
